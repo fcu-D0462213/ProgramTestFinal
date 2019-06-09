6f05964d64d7ce68967396332785813020c9a80b
JiaZe <920687534@qq.com>
2019-06-09 12:29:24 +0800

    修改同分 减少复杂度

--- enrollsystem/School.java
+++ enrollsystem/School.java
@@ -9,93 +9,104 @@ import java.util.TreeSet;
  *
  * */
 public class School {
-  private String schoolName;// 學校名字
-  private int enrollNum;// 錄取人數
-  private int standbyEnrollNum;// 備取人數
-  private double minEnrollScore;// 最低錄取分數線
-  private int totalStuNum = 0;// 报名人数
-  TreeSet<Student> studentsList = new TreeSet<>();// 每個學校的報考學生列表
-  TreeSet<Student> enrollList = new TreeSet<>();// 每個學校的录取學生列表
-  TreeSet<Student> standbyEnrollList = new TreeSet<>();// 每個學校的备取學生列表
-  TreeSet<Student> outEnrollList = new TreeSet<>();// 每個學校的未录取學生列表
-
-  public School(String schoolName, int enrollNum, int standbyEnrollNum, double minEnrollScore) {
-    this.schoolName = schoolName;
-    this.enrollNum = enrollNum;
-    this.standbyEnrollNum = standbyEnrollNum;
-    this.minEnrollScore = minEnrollScore;
-  }
-
-  public void setSchoolStuList() {
-    Iterator<Student> stuIte = studentsList.iterator();
-    Student stu;
+    private String schoolName;// 學校名字
+    private int enrollNum;// 錄取人數
+    private int standbyEnrollNum;// 備取人數
+    private double minEnrollScore;// 最低錄取分數線
+    private int totalStuNum = 0;// 报名人数
+    TreeSet<Student> studentsList = new TreeSet<>();// 每個學校的報考學生列表
+    TreeSet<Student> enrollList = new TreeSet<>();// 每個學校的录取學生列表
+    TreeSet<Student> standbyEnrollList = new TreeSet<>();// 每個學校的备取學生列表
+    TreeSet<Student> outEnrollList = new TreeSet<>();// 每個學校的未录取學生列表
     double enrollMinScore = 0;
     double standbyMinScore = 0;
-    for (int i = 0; i < enrollNum; i++) {
-      if (stuIte.hasNext()) {
-        stu = stuIte.next();
-        if (stu.getStuScore() >= this.minEnrollScore)
-          enrollList.add(stu);
-        else
-          outEnrollList.add(stu);
-        enrollMinScore = stu.getStuScore();
-      }
+
+    public School(String schoolName, int enrollNum, int standbyEnrollNum, double minEnrollScore) {
+        this.schoolName = schoolName;
+        this.enrollNum = enrollNum;
+        this.standbyEnrollNum = standbyEnrollNum;
+        this.minEnrollScore = minEnrollScore;
     }
 
-    for (int i = 0; i < standbyEnrollNum; i++) {
-      if (stuIte.hasNext()) {
-        stu = stuIte.next();
-        // 檢測正取同分
+    public Student checkSameEnrollScore(Student stu, Iterator<Student> stuIte) {
         while (stu.getStuScore() == enrollMinScore) {
-          enrollList.add(stu);
-          enrollNum++;
-          stu = stuIte.next();
+            enrollList.add(stu);
+            enrollNum++;
+            stu = stuIte.next();
         }
-        if (stu.getStuScore() >= this.minEnrollScore)
-          standbyEnrollList.add(stu);
-        else
-          outEnrollList.add(stu);
-        standbyMinScore = stu.getStuScore();
-      }
+        return stu;
     }
-    for (int i = 0; i < totalStuNum - enrollNum - standbyEnrollNum; i++) {
-      if (stuIte.hasNext()) {
-        stu = stuIte.next();
-        // 檢測正取同分
-        while (stu.getStuScore() == enrollMinScore) {
-          enrollList.add(stu);
-          enrollNum++;
-          stu = stuIte.next();
-        }
-        // 檢測備取同分
+
+    public Student checkSameStandbyScore(Student stu, Iterator<Student> stuIte) {
         while (stu.getStuScore() == standbyMinScore) {
-          standbyEnrollList.add(stu);
-          standbyEnrollNum++;
-          stu = stuIte.next();
+            standbyEnrollList.add(stu);
+            standbyEnrollNum++;
+            stu = stuIte.next();
         }
-        outEnrollList.add(stu);
-      }
+        return stu;
     }
-  }
 
-  public String getSchoolName() {
-    return schoolName;
-  }
+    public void setlowerScoreList() {
+        Iterator<Student> stuIte = studentsList.iterator();
+        Student stu;
+        if (stuIte.hasNext()) {
+            stu = stuIte.next();
+            if (stu.getStuScore() < this.minEnrollScore)
+                outEnrollList.add(stu);
+        }
+    }
 
-  public int getEnrollNum() {
-    return enrollNum;
-  }
+    public void setSchoolStuList() {
+        setlowerScoreList();
+        Iterator<Student> stuIte = studentsList.iterator();
+        Student stu;
+        for (int i = 0; i < enrollNum; i++) {
+            if (stuIte.hasNext()) {
+                stu = stuIte.next();
+                enrollList.add(stu);
+                enrollMinScore = stu.getStuScore();
+            }
+        }
 
-  public int getStandbyEnrollNum() {
-    return standbyEnrollNum;
-  }
+        for (int i = 0; i < standbyEnrollNum; i++) {
+            if (stuIte.hasNext()) {
+                stu = stuIte.next();
+                // 檢測正取同分
+                stu = checkSameEnrollScore(stu, stuIte);
+                standbyEnrollList.add(stu);
+                standbyMinScore = stu.getStuScore();
+            }
+        }
+        for (int i = 0; i < totalStuNum - enrollNum - standbyEnrollNum; i++) {
+            if (stuIte.hasNext()) {
+                stu = stuIte.next();
+                // 檢測正取同分
+                stu = checkSameEnrollScore(stu, stuIte);
+                // 檢測備取同分
+                stu = checkSameStandbyScore(stu, stuIte);
+                outEnrollList.add(stu);
+            }
+        }
+    }
 
-  public int getTotalStuNum() {
-    return totalStuNum;
-  }
+    public String getSchoolName() {
+        return schoolName;
+    }
 
-  public void addStuNum() {
-    this.totalStuNum++;
-  }
+    public int getEnrollNum() {
+        return enrollNum;
+    }
+
+    public int getStandbyEnrollNum() {
+        return standbyEnrollNum;
+    }
+
+    public int getTotalStuNum() {
+        return totalStuNum;
+    }
+
+    public void addStuNum() {
+        this.totalStuNum++;
+    }
 
 }
